c80d2b24ef4d06cb72a64e9049979a3798458082
dev.sagartmg <dev.sagartmg@gmail.com>
2026-02-10 12:57:08 +0545

    todos edit using useState

--- my-react-project/src/Todos.jsx
+++ my-react-project/src/Todos.jsx
@@ -10,7 +10,6 @@ function Todos() {
       title: "express",
       status: false,
     },
-    ,
     {
       title: "html",
       status: true,
@@ -25,32 +24,90 @@ function Todos() {
     },
   ]);
 
+  const [editableTodo, setEditableTodo] = useState(null);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    e.target;
-    console.log(e.target.title.value);
+    // e.target; //<form>
+    // console.log(e.target.title.value);
 
     // todos.push({
     //   title: e.target.title.value,
-    //   status: false,
     // });
+    //  ERROR: cannot change sate variable directly
 
-    // setTodos(pass the array) // the array should contain old values as well as new value
+    // let temp =  todos  // XX error: casuse temp will point to todos original location. && todos is a state && state cant be changed directly.
 
-    setTodos([
-      {
-        title: "node",
-        status: false,
-      },
-      {
-        title: "express",
-        status: false,
-      },
-    ]);
+    let temp = [...todos]; // spread operator // copy previous todos
+    temp.push({
+      title: e.target.title.value,
+      status: false,
+    });
 
-    console.log(todos);
+    setTodos(temp);
   };
 
+  const deleteTodo = (index) => {
+    let temp = [...todos];
+    // ...delete partiruclar index from temp todos
+    /* 
+    const [todos, setTodos] = useState([
+    {
+      title: "node",
+      status: false,
+    },
+    {
+      title: "express",
+      status: false,
+    },
+    {
+      title: "html",
+      status: true,
+    },
+    
+    */
+
+    // let filteredTodos = temp.filter((el, idx) => {
+    //   if (index == idx) {
+    //     return false;
+    //   }
+    //   return true;
+    // });
+    // setTodos(filteredTodos);
+
+    temp.splice(index, 1);
+    setTodos(temp);
+  };
+
+  const editTodo = (index, title, status) => {
+    setEditableTodo({
+      index,
+      title,
+      status,
+    });
+  };
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    let temp = [...todos];
+
+    temp = temp.map((el, index) => {
+      if (index == editableTodo.index) {
+        return {
+          title: e.target.title.value,
+          status: e.target.status.checked,
+        };
+      }
+      return el;
+    });
+
+    setTodos(temp);
+    setEditableTodo(null);
+  };
+
+  console.log({ todos });
+
+  // re-render
   return (
     <div className="white-space">
       <form onSubmit={handleSubmit}>
@@ -66,22 +123,69 @@ function Todos() {
           <th>status</th>
           <th>Action</th>
         </thead>
-        {todos.map((el, index) => {
-          return (
-            <tr className={`todo-item ${el.status ? "completed" : "pending"}`}>
-              <td>{index + 1}</td>
-              <td>{el.title}</td>
-              <td>{el.status ? "completed" : "pending"}</td>
-              <td>
-                <div>
-                  <button>edit</button>&nbsp;
-                  <button>delete</button>
-                </div>
-              </td>
-            </tr>
-          );
-        })}
+        <tbody>
+          {todos.map((el, index) => {
+            return (
+              <tr className={`todo-item ${el.status ? "completed" : "pending"}`}>
+                <td>{index + 1}</td>
+                <td>{el.title}</td>
+                <td>{el.status ? "completed" : "pending"}</td>
+                <td>
+                  <div>
+                    <button
+                      onClick={() => {
+                        editTodo(index, el.title, el.status);
+                      }}
+                    >
+                      edit
+                    </button>
+                    &nbsp;
+                    <button
+                      onClick={() => {
+                        deleteTodo(index);
+                      }}
+                    >
+                      delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
+
+      {/*   modal */}
+      {editableTodo != null && (
+        <div
+          className="edit-modal"
+          onClick={() => {
+            setEditableTodo(null);
+          }}
+        >
+          <form
+            className="edit-form"
+            onClick={(e) => {
+              // e.preventDefault();
+              e.stopPropagation();
+            }}
+            onSubmit={handleEdit}
+          >
+            <div>
+              <label>Ttile</label>
+              <br />
+              <input placeholder="title" name="title" defaultValue={editableTodo.title} />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="status">Status</label>
+              <input id="status" type="checkbox" name="status" defaultChecked={editableTodo.status} />
+            </div>
+            <br />
+            <input type="submit" />
+          </form>
+        </div>
+      )}
     </div>
   );
 }
--- my-react-project/z_temp.js
+++ my-react-project/z_temp.js
@@ -1,39 +1,102 @@
-todos = [
-    {
-        title: "css",
-        status: false,
-    },
-    {
-        title: "react",
-        status: false,
-    },
-    {
-      title: "html",
-      status: true,
-    },
-];
+// // todos = [
+// //   {
+// //     title: "css",
+// //     status: false,
+// //   },
+// //   {
+// //     title: "react",
+// //     status: false,
+// //   },
+// //   {
+// //     title: "html",
+// //     status: true,
+// //   },
+// // ];
 
-todos.filter((el)=> {
-    if(el.status){
-        return true
-    }
-})
+// // todos.filter((el) => {
+// //   if (el.status) {
+// //     return true;
+// //   }
+// // });
 
-todos.filter((el)=> el.status)
+// // todos.filter((el) => el.status);
 
-todos = [
+// // todos = [
+// //   {
+// //     title: "html",
+// //     status: true,
+// //   },
+// // ];
+
+// // todos = [
+// //   <tr class="todo-item pending">
+// //     <td>{index + 1}</td>
+// //     <td>{el.title}</td>
+// //     <td>{el.status ? "completed" : "pending"}</td>
+// //   </tr>,
+// //   undefined,
+// // ];
+
+// let fullName = "ram";
+// let user = fullName;
+// user = "hari";
+
+// console.log({ user });
+// console.log({ fullName });
+
+// let person = {
+//   name: "ram",
+//   age: 12,
+//   address: "kathmandu",
+// };
+
+// let person2 = person;
+// person2.name = "hari";
+
+// let person3 = { ...person };
+// person3.name = "sita kumari";
+
+// console.log({ person3 });
+// console.log({ person2 });
+// console.log({ person });
+
+// let todos = ["html", "css", "mongodbd"];
+
+// let temp = todos;
+// temp.push("react");
+
+// let tempArr = [...todos];
+// tempArr.push("database");
+
+// console.log({ tempArr });
+// console.log({ temp });
+// console.log({ todos });
+
+let todos = [
   {
     title: "html",
     status: true,
   },
+  {
+    title: "css",
+    status: true,
+  },
+  {
+    title: "express",
+    status: false,
+  },
 ];
 
 
-todos = [
-  <tr class="todo-item pending">
-    <td>{index + 1}</td>
-    <td>{el.title}</td>
-    <td>{el.status ? "completed" : "pending"}</td>
-  </tr>,
-  undefined,
-];
+// todos.forEach(el =>{})
+// let modifiedTodos = todos.map((el) =>{ return {title:el.title,status:el.status?"completed":"income"} }) 
+// console.log(modifiedTodos);
+
+let filteredTodos = todos.find(el =>{
+  if(el.status){
+    return true
+  }
+  return false
+  // return false
+})
+console.log(filteredTodos);
