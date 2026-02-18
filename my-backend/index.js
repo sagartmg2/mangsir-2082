2072ecdf0291db0d1f11fda24de4120d74aa95f7
dev.sagartmg <dev.sagartmg@gmail.com>
2026-02-18 10:12:35 +0545

    feat: create todos get and post api

--- my-backend/index.js
+++ my-backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express"); // common js
 // import express from "express" // es module
+const cors = require("cors");
 
 const app = express();
+app.use(cors()); // global middleware
+app.use(express.json()); // global middleware
+
 const port = 3000;
 
 app.get("/api", (req, res) => {
@@ -10,9 +14,40 @@ app.get("/api", (req, res) => {
 
 app.get("/api/products", (req, res) => {
   res.send({
-    products: [{ title: "bag" }, { title: "phone" },{ title: "samsung phone" }],
+    products: [{ title: "bag" }, { title: "phone" }, { title: "samsung phone" }],
   });
 });
+
+/* 
+  C - create
+  R  read
+  U  update 
+  D  delete
+*/
+
+let dbTodos = [
+  { title: "html", status: true },
+  { title: "css", status: true },
+  { title: "react", status: true },
+  { title: "express", status: false },
+];
+
+app.get("/api/todos", (req, res) => {
+  res.send(dbTodos);
+});
+
+app.post("/api/todos", (req, res) => {
+  dbTodos.push({
+    title: req.body.title,
+    status: false,
+  });
+
+  res.send({
+    msg: "todos crated",
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
